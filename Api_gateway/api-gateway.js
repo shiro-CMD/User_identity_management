587155f23e0d4f1a305408315180f0727043273d
shiro-CMD <toruleng@gmail.com>
2026-09-17 16:13:07 +0700

    Route gateway services to separate EC2 hosts

--- Api_gateway/api-gateway.js
+++ Api_gateway/api-gateway.js
@@ -6,6 +6,10 @@ const app = express();
 const httpProxy = require('http-proxy')
 const proxy = httpProxy.createProxyServer();
 const serviceHost = process.env.SERVICE_HOST || 'localhost';
+const loginHost = process.env.LOGIN_SERVICE_HOST || serviceHost;
+const registrationHost = process.env.REGISTRATION_SERVICE_HOST || serviceHost;
+const adminHost = process.env.ADMIN_SERVICE_HOST || serviceHost;
+const userHost = process.env.USER_SERVICE_HOST || serviceHost;
 const jwtSecret = process.env.JWT_SECRET || 'local-development-secret';
 
 function requireJwt(req, res, next) {
@@ -48,26 +52,26 @@ proxy.on('error', (error, req, res) => {
 //REDIRECT TO THE STUDENT MICROSERVICE
 app.use('/login', (req, res) => {
     console.log("INSIDE API GATEWAY STUDENT ROUTE")
-    proxy.web(req, res, { target: `http://${serviceHost}:5004` });
+    proxy.web(req, res, { target: `http://${loginHost}:5004` });
 })
 
 app.use('/reg', (req, res) => {
     console.log("INSIDE API GATEWAY REG ROUTE")
-    proxy.web(req, res, { target: `http://${serviceHost}:5003` });
+    proxy.web(req, res, { target: `http://${registrationHost}:5003` });
 })
 app.use(['/ad', '/admin'], requireAdmin);
 app.use('/ad', (req, res) => {
     console.log("INSIDE API GATEWAY ADMIN ROUTE")
-    proxy.web(req, res, { target: `http://${serviceHost}:5001` });
+    proxy.web(req, res, { target: `http://${adminHost}:5001` });
 })
 app.use('/admin', (req, res) => {
     console.log("INSIDE API GATEWAY ADMIN ROUTE")
-    proxy.web(req, res, { target: `http://${serviceHost}:5001` });
+    proxy.web(req, res, { target: `http://${adminHost}:5001` });
 })
 app.use(['/user/viewprofile', '/user/updateprofile'], requireJwt);
 app.use('/user', (req, res) => {
     console.log("INSIDE API GATEWAY USER ROUTE")
-    proxy.web(req, res, { target: `http://${serviceHost}:5002` });
+    proxy.web(req, res, { target: `http://${userHost}:5002` });
 })
 
 app.listen(5005, () => {
